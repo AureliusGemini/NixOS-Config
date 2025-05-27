6fb93ac191bf9dd5b3e16de37f80d1b2b5466105
AureliusGemini <AureliusGemini@users.noreply.github.com>
2025-05-28 02:37:12 +0700

    Auto-backup: 2025-05-28 02:37:12

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -8,8 +8,8 @@
   # Enable KVM/QEMU virtualization
   virtualisation.libvirtd = {
     enable = true;
-    qemuPackage = pkgs.qemu_kvm;
-    qemuRunAsRoot = false;
+    qemu.package = pkgs.qemu_kvm;
+    qemu.runAsRoot = false;
     onBoot = "ignore";
     onShutdown = "shutdown";
   };
