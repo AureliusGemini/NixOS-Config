fbf48544ecd856a7258d97aca3a616dba0596548
AureliusGemini <aureliusgemini@gmail.com>
2025-06-07 05:00:39 +0700

    test

--- services/rustdesk.nix
+++ services/rustdesk.nix
@@ -1,12 +1,19 @@
-(config, pkgs, ... );
+# /etc/nixos/services/rustdesk.nix
+{ config, pkgs, ... }:
 
 {
-    services.rustdesk-server.enable = true;
-    services.rustdesk-server.openFirewall = true;
-    services.rustdesk-server.package = pkgs.rustdesk-server;
-    services.rustdesk-server.relay.enable = true;
-    # services.rustdesk-server.relay.extraArgs = [ "--relay-hosts=relay1.example.com,relay2.example.com" ];
-    services.rustdesk-server.signal.enable = true;
-    # services.rustdesk-server.signal.extraArgs = [ "--relay-hosts=signal1.example.com,signal2.example.com" ];
-    # services.rustdesk-server.signal.relayHosts = [ "signal1.example.com" "signal2.example.com" ];
+  services.rustdesk-server = {
+    enable = true;
+    openFirewall = true;
+    package = pkgs.rustdesk-server;
+    relay = {
+      enable = true;
+      # extraArgs = [ "--relay-hosts=relay1.example.com,relay2.example.com" ];
+    };
+    signal = {
+      enable = true;
+      # extraArgs = [ "--relay-hosts=signal1.example.com,signal2.example.com" ];
+      # relayHosts = [ "signal1.example.com" "signal2.example.com" ];
+    };
+  };
 }
