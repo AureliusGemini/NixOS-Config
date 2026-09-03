981a080da2feed784565e5c472fc707fb3979a2f
AureliusGemini <93374856+AureliusGemini@users.noreply.github.com>
2026-09-03 22:49:46 +0700

    System update & flake sync: 2026-09-03 22:49

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -61,7 +61,7 @@
 #   # PipeWire Audio Stack
   security.rtkit.enable = true;
   services.pipewire = {
-#     enable = true;
+    enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
