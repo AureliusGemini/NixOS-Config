e576278d5ddadb320792b4d2f68eb4baae5dabbf
AureliusGemini <93374856+AureliusGemini@users.noreply.github.com>
2026-09-21 09:48:50 +0000

    nix-ld + podman

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -18,15 +18,23 @@
     enable = true;
     defaultUser = "aurelius";
   };
+  programs.nix-ld.enable = true;
 
   users.users.aurelius = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ];
+    extraGroups = [ "wheel" "podman" ];
     description = "AureliusGemini";
   };
 
   security.sudo.wheelNeedsPassword = false;
-
+  
+  virtualisation.podman = {
+    enable = true;
+    dockerCompat = true;         # Creates the docker alias and socket
+    defaultNetwork.settings = {
+    dns_enabled = true;
+    };
+  };
   programs.git.enable = true;
 
   environment.systemPackages = with pkgs; [
